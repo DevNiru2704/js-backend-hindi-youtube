3102cba97c374219881b167645f8bbd227c6c793
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-08-01 13:03:49 +0000

    Learned how to connect mongodb with backend

---
+++ 05_chaiAurBackend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"})) //Take limited amount of cookie data because we don't want our website to crash.
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+})) 
+
+app.use(expres.static("public")) //This is to tell express that if I want to keep some assets such as images,svg,video,etc.,
+                                 //I will keep it in the public folder.
+
+app.use(cookieParser()) //This allows us to configure cookies.
+
+export {app}
---
+++ 05_chaiAurBackend/src/index.js
@@ -0,0 +1,53 @@
+// require("dotenv").config({path:"../env"}) //It ruins the consistency of our program because it is not yet possible to use es6 yet.
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import {} from "./constants.js";
+import {connectDB} from "./db/index.js"
+import { app } from "./app.js";
+
+dotenv.config({
+    path:"../env"
+})
+
+//connectDB returns a promise because connectDB is an async await function
+const port=process.env.PORT||3000
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+    })
+    app.listen(port,()=>{
+        console.log(`Server is running at port:${port}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed: ",error)
+}) 
+
+
+
+
+
+
+
+
+
+/*
+const app=express();
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error);
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.error("ERROR: ",error);
+        throw error
+    }
+})();
+*/
