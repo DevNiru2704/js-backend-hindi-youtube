9de2a0e2de919a1cef0517a13816c3117ddd200a
DevNiru2704 <nirmalyamandal342@gmail.com>
2024-08-18 09:55:18 +0530

    Add user account update functionality

--- 05_chaiAurBackend/src/controllers/user.controller.js
+++ 05_chaiAurBackend/src/controllers/user.controller.js
@@ -258,9 +258,109 @@ const getCurrentUser=asyncHandler(async (req,res)=>{
     )
 })
 
+const updateUserAccount=asyncHandler(async (req,res)=>{
+    const {fullName,username,email}=req.body
+    if(!fullName||!username||!email){
+        throw new ApiError("401","All fields are required!")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullName,
+                username,
+                email
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "Account details updated successfully!"
+    ))
+})
+
+const updateUserAvatar=asyncHandler(async (req,res)=>{
+    const avatarLocalPath=req.file?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar Path does not exist")
+    }
+
+    console.log(avatarLocalPath)
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading avatar!")
+    }
+    
+    const user=await User.findByIdAndUpdate(
+        user._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "Avatar updated successfully!"
+    ))
+})
+
+const updateUserCoverImage=asyncHandler(async (req,res)=>{
+    const coverImageLocalPath=req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"Cover Image Path does not exist")
+    }
+
+    console.log(coverImageLocalPath)
+
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400,"Error while uploading cover image!")
+    }
+    
+    const user=await User.findByIdAndUpdate(
+        user._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        user,
+        "Cover Image updated successfully!"
+    ))
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAccount,
+    updateUserAvatar,
+    updateUserCoverImage
 }
