b71f143fb3097855fa806882487ab6c50a9559d5
DevNiru2704 <nirmalyamandal342@gmail.com>
2024-08-23 19:22:45 +0530

    Add new routes and controllers for user account management

--- 05_chaiAurBackend/src/routes/user.route.js
+++ 05_chaiAurBackend/src/routes/user.route.js
@@ -1,33 +1,54 @@
 import {Router} from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserAccount,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
-import { verifyJWT  } from "../middlewares/auth.middleware.js";
-const router = Router()
+import {verifyJWT} from "../middlewares/auth.middleware.js";
+const router = Router();
 
 // router.route("/register").post(registerUser) //Now here /users will work as a prefix and the great thing about this is we dont have to write /users again and again. Our website will be formed as http://localhost:8000/users/register
 
-router.route("/register").post( //We use multer here because res.body cannot handle data coming from an url
+router.route("/register").post(
+    //We use multer here because res.body cannot handle data coming from an url
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1,
         },
         {
-            name:"coverImage",
-            maxCount:1
-        }
+            name: "coverImage",
+            maxCount: 1,
+        },
     ]),
     registerUser
-)
+);
 
-router.route("/login").post(loginUser)// router.route("login").post(registerUser) //http://localhost:8000/users/login. We can avoid writing the same code again and again using this process
+router.route("/login").post(loginUser); // router.route("login").post(registerUser) //http://localhost:8000/users/login. We can avoid writing the same code again and again using this process
 
 //secure route
 
-router.route("/logout").post(verifyJWT ,logoutUser)
-router.route("refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateUserAccount);
+router
+    .route("/update-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/update-cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
-
-
-
-export default router
+export default router;
